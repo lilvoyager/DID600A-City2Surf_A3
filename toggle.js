ab83666bd4e442de48bbfb11f346f972e647a9ed
lilvoyager <AnnieKy@Annies-MacBook-Pro.local>
2021-04-25 00:56:29 +1000

    Fix hamburger menu and nav

--- toggle.js
+++ toggle.js
@@ -1,4 +1,13 @@
-// JavaScript Document/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+// JavaScript Document
+
+/* 
+    Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon.
+    Also toggles the hamburger menu to switch to a cross icon when menu is open, based on
+    https://jonsuh.com/hamburgers/
+*/
+function toggleMenu() {
+    const topnav = document.getElementsByClassName("topnav")[0];
+    topnav.classList.toggle("responsive"); // toggle the menu
+    const hamburger = document.getElementById("hamburger");
+    hamburger.classList.toggle("is-active"); // toggle the cross and hamburger menu
 }
