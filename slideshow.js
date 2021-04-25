d1bbbbaa836cafa4ea3915707ee688a89a645723
lilvoyager <AnnieKy@Annies-MacBook-Pro.local>
2021-04-25 14:51:33 +1000

    Fix slideshow

--- slideshow.js
+++ slideshow.js
@@ -1,16 +1,18 @@
 // This solution is based on https://www.w3schools.com/w3css/w3css_slideshow.asp
-
 var slideIndex = 0;
-carousel();
+// have to wait for the images to load into the document first before executing the function.
+// explanation found here - https://stackoverflow.com/questions/32222255/cant-access-the-value-of-htmlcollection
+window.addEventListener('load', carousel);
 
 function carousel() {
-  var i;
   var x = document.getElementsByClassName("hero_slideshow");
-  for (i = 0; i < x.length; i++) {
+  for (let i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+  if (slideIndex > x.length) {
+    slideIndex = 1
+  }
+  x[slideIndex - 1].style.display = "block";
+  setTimeout(carousel, 3000); // Change image every 3 seconds
 }
